c3a31d71486379aaf2ad5909ec44feb454827013
McCrearyD <dyllanmccreary@gmail.com>
2019-01-27 19:23:11 -0800

    Made file more functional & added save weights on exit

--- quality_recognition_model.py
+++ quality_recognition_model.py
@@ -2,29 +2,41 @@ from convert_data import SPECTROGRAM_DATA_DIR_PATH, get_chord_data_paths
 from fastai.vision import *
 from fastai.metrics import error_rate
 import os
+import atexit
+
 
 MODEL_NAME = 'model-1'
 
-bs = 5
-epochs = 50
 
-# Load data
-data = np.array(get_chord_data_paths(parent_path=SPECTROGRAM_DATA_DIR_PATH))
-data = ImageDataBunch.from_lists(
-    path=SPECTROGRAM_DATA_DIR_PATH,
-    fnames=data[:, 2], labels=data[:, 1],
-    bs=bs
-).normalize(imagenet_stats)
+def get_learner(bs=5):
+    """
+    Returns the quality recognition model
+    """
 
-learn = create_cnn(data, models.resnet34, metrics=error_rate)
+    # Load data
+    data = get_chord_data_paths(parent_path=SPECTROGRAM_DATA_DIR_PATH)
+    data = ImageDataBunch.from_lists(
+        path=SPECTROGRAM_DATA_DIR_PATH,
+        fnames=data[:, 2], labels=data[:, 1],
+        bs=bs
+    ).normalize(imagenet_stats)
 
-# load if model exists
-if os.path.isfile(SPECTROGRAM_DATA_DIR_PATH + '/models/%s.pth' % MODEL_NAME):
-    learn.load(MODEL_NAME)
+    print('Data loaded, available classes: ', data.classes)
 
-learn.fit_one_cycle(epochs)
+    learn = create_cnn(data, models.resnet34, metrics=error_rate)
 
-interp = ClassificationInterpretation.from_learner(learn)
-print(interp.most_confused())
+    # load if model exists
+    if os.path.isfile(SPECTROGRAM_DATA_DIR_PATH + '/models/%s.pth' % MODEL_NAME):
+        learn.load(MODEL_NAME)
 
-learn.save(MODEL_NAME)
+    return learn
+
+
+if __name__ == '__main__':
+    learn = get_learner()
+    learn.fit_one_cycle(50)
+
+    interp = ClassificationInterpretation.from_learner(learn)
+    print(interp.most_confused())
+
+    atexit.register(lambda: learn.save(MODEL_NAME))
