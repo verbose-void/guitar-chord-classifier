7c794bbebee28b7d4a95d1e2ab3905933fc754af
McCrearyD <dyllanmccreary@gmail.com>
2019-02-02 12:31:02 -0800

    Add a train & validation set for the chord recognition model

--- chord_recognition_model.py
+++ chord_recognition_model.py
@@ -3,29 +3,31 @@ from fastai.vision import *
 from fastai.metrics import error_rate
 import os
 import atexit
-
+import numpy as np
 
 MODEL_NAME = 'model-1'
 
 
-def get_learner(bs=5):
+def get_learner(bs=5, seed=None):
     """
     Returns the quality recognition model
     """
 
-    # Load data
-    data = get_chord_data_paths(
-        parent_path=SPECTROGRAM_DATA_DIR_PATH, exempt_qualities=['ringy'])
-    data = ImageDataBunch.from_lists(
-        path=SPECTROGRAM_DATA_DIR_PATH,
-        fnames=data[:, 2], labels=data[:, 0],
-        bs=bs
-    ).normalize(imagenet_stats)
+    if seed is not None:
+        np.random.seed(seed)
 
+    # Load data
+    # data = get_chord_data_paths(
+    #     parent_path=SPECTROGRAM_DATA_DIR_PATH, exempt_qualities=['ringy'])
+    # data = ImageDataBunch.from_lists(
+    #     path=SPECTROGRAM_DATA_DIR_PATH,
+    #     fnames=data[:, 2], labels=data[:, 0],
+    #     bs=bs
+    # ).normalize(imagenet_stats)
+    data = ImageDataBunch.from_folder('cp_spec_data')
     print('Data loaded, available classes: ', data.classes)
 
     learn = create_cnn(data, models.resnet34, metrics=(error_rate, accuracy))
-
     # load if model exists
     # if os.path.isfile(SPECTROGRAM_DATA_DIR_PATH + '/models/%s.pth' % MODEL_NAME):
     #     learn.load(MODEL_NAME)
@@ -35,9 +37,8 @@ def get_learner(bs=5):
 
 if __name__ == '__main__':
     learn = get_learner()
-    learn.fit_one_cycle(50)
+    atexit.register(lambda: learn.save(MODEL_NAME))
+    learn.fit_one_cycle(5)
 
-    interp = ClassificationInterpretation.from_learner(learn)
-    print(interp.most_confused())
-
-    # atexit.register(lambda: learn.save(MODEL_NAME))
+    # interp = ClassificationInterpretation.from_learner(learn)
+    # print(interp)
